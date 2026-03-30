f117f0ded845d23ac7c4d5b05d8ba7cfef79a565
Gautam Kumar <gk4946863@gmail.com>
2026-03-30 18:36:58 +0530

    Added 7-day forecast with clickable cards and UI highlight

--- index.js
+++ index.js
@@ -35,6 +35,72 @@ async function success(position){
    updateUI(result)}
 
 
+
+// CREATING A FORECAST FUNCTION
+
+function showForecast(days){
+    const container = document.getElementById("forecast_container");
+    container.innerHTML = ""; // clear old data.
+
+    days.forEach(day => {
+        const date = new Date(day.date);
+        const dayName = date.toLocaleDateString("en-US", { weekday: "short" });
+
+const card = document.createElement("div");
+        card.classList.add("forecast_card");
+
+
+        card.innerHTML += `
+                <p>${dayName}</p>
+                <img src="https:${day.day.condition.icon}" width="50"/>
+                <p>${day.day.maxtemp_c}° / ${day.day.mintemp_c}°</p>
+        `;
+  card.addEventListener("click", () => {
+            updateDayWeather(day);   // update main UI
+            highlightCard(index);   // highlight clicked
+        });
+
+        container.appendChild(card);
+
+    });
+}
+
+
+
+
+function updateDayWeather(day){
+    citytemp.innerText = `${day.day.avgtemp_c}°C`;
+    weather_condition_text.innerText = day.day.condition.text;
+    icon.src = `https:${day.day.condition.icon}`;
+
+    // forecast API daily data
+    humidity.innerText = `Humidity: ${day.day.avghumidity} %`;
+    wind.innerText = `Wind: ${day.day.maxwind_kph} km/h`;
+}
+
+
+
+
+
+
+function highlightCard(selectedIndex){
+    const cards = document.querySelectorAll(".forecast_card");
+
+    cards.forEach((card, index) => {
+        if(index === selectedIndex){
+            card.style.background = "#4CAF50";
+            card.style.color = "white";
+        } else {
+            card.style.background = "white";
+            card.style.color = "black";
+        }
+    });
+}
+
+
+
+
+
    function updateUI(result) {
     cityname.innerText=`${result.location.name},${result.location.region}`
   citytemp.innerText=`${result.current.temp_c}°C`
@@ -46,13 +112,17 @@ async function success(position){
   wind.innerText=`Wind: ${result.current.wind_kph} km/h`
     pressure.innerText= `Pressure: ${result.current.pressure_mb} mb`
 uv.innerText=`UV Index: ${result.current.uv}`
+showForecast(result.forecast.forecastday)
    }
+
+
+
 
 function failure(){
 alert("Location permission denied")
 }
 async function weather(query){
-        const weather_data=await fetch(`https://api.weatherapi.com/v1/current.json?key=edf357f32cc846d5946183041261903&q=${query}&aqi=yes`) 
+        const weather_data=await fetch(`https://api.weatherapi.com/v1/forecast.json?key=edf357f32cc846d5946183041261903&q=${query}&days=7`) 
     const data=await weather_data.json()
     return data
 }
